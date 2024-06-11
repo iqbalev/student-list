a390827ac477d50567affa4808f43c023cc45ea0
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-06-11 21:42:12 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Inisialisasi variabel
+  const containerDiv = document.querySelector(".container");
+  const inputForm = document.querySelector(".input-form");
+
+  // Function untuk menambah data ke list
+  function addToList() {
+    const fullNameInput = document.getElementById("full-name").value;
+    const studentIdInput = document.getElementById("student-id").value;
+    const majorInput = document.getElementById("major").value;
+
+    console.log(`Nama Lengkap: ${fullNameInput}`);
+    console.log(`Nomor Induk Mahasiswa: ${studentIdInput}`);
+    console.log(`Program Studi: ${majorInput}`);
+    console.log(`\n`);
+
+    const listContainerDiv = document.createElement("div");
+    listContainerDiv.classList.add("list-container");
+
+    const listContainerH2 = document.createElement("h2");
+    listContainerH2.textContent = "Daftar Mahasiswa";
+
+    const studentInformationDiv = document.createElement("div");
+    studentInformationDiv.classList.add("student-information");
+
+    const fullNameDiv = document.createElement("div");
+    fullNameDiv.classList.add("full-name");
+    fullNameDiv.innerText = "Nama Lengkap";
+
+    const fullNameP = document.createElement("p");
+
+    const studentIdDiv = document.createElement("div");
+    studentIdDiv.classList.add("student-id");
+    studentIdDiv.innerText = "Nomor Induk Mahasiswa";
+
+    const studentIdP = document.createElement("p");
+
+    const majorDiv = document.createElement("div");
+    majorDiv.classList.add("major");
+    majorDiv.innerText = "Program Studi";
+
+    const majorP = document.createElement("p");
+
+    fullNameP.textContent = `${fullNameInput}`;
+    studentIdP.textContent = `${studentIdInput}`;
+    majorP.textContent = `${majorInput}`;
+
+    fullNameDiv.appendChild(fullNameP);
+    studentIdDiv.appendChild(studentIdP);
+    majorDiv.appendChild(majorP);
+
+    studentInformationDiv.appendChild(fullNameDiv);
+    studentInformationDiv.appendChild(studentIdDiv);
+    studentInformationDiv.appendChild(majorDiv);
+
+    listContainerDiv.appendChild(listContainerH2);
+    listContainerDiv.appendChild(studentInformationDiv);
+
+    containerDiv.appendChild(listContainerDiv);
+  }
+
+  // Menjalankan event
+  inputForm.addEventListener("submit", function (event) {
+    addToList();
+    event.preventDefault();
+  });
+});
