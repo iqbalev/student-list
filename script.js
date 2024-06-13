a0b352e16ad2dacdf56931485ee44f9e6e1e251b
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-06-13 14:35:46 +0700

    second commit

--- script.js
+++ script.js
@@ -3,42 +3,40 @@ document.addEventListener("DOMContentLoaded", function () {
   const containerDiv = document.querySelector(".container");
   const inputForm = document.querySelector(".input-form");
 
+  const listContainerDiv = document.createElement("div");
+  listContainerDiv.classList.add("list-container");
+
+  const listContainerH2 = document.createElement("h2");
+  listContainerH2.textContent = "DAFTAR MAHASISWA";
+
+  const studentInformationDiv = document.createElement("div");
+  studentInformationDiv.classList.add("student-information");
+
+  const fullNameDiv = document.createElement("div");
+  fullNameDiv.classList.add("full-name");
+  fullNameDiv.innerText = "Nama Lengkap";
+
+  const studentIdDiv = document.createElement("div");
+  studentIdDiv.classList.add("student-id");
+  studentIdDiv.innerText = "Nomor Induk Mahasiswa";
+
+  const majorDiv = document.createElement("div");
+  majorDiv.classList.add("major");
+  majorDiv.innerText = "Program Studi";
+
   // Function untuk menambah data ke list
   function addToList() {
     const fullNameInput = document.getElementById("full-name").value;
     const studentIdInput = document.getElementById("student-id").value;
     const majorInput = document.getElementById("major").value;
 
-    console.log(`Nama Lengkap: ${fullNameInput}`);
-    console.log(`Nomor Induk Mahasiswa: ${studentIdInput}`);
-    console.log(`Program Studi: ${majorInput}`);
-    console.log(`\n`);
-
-    const listContainerDiv = document.createElement("div");
-    listContainerDiv.classList.add("list-container");
-
-    const listContainerH2 = document.createElement("h2");
-    listContainerH2.textContent = "Daftar Mahasiswa";
-
-    const studentInformationDiv = document.createElement("div");
-    studentInformationDiv.classList.add("student-information");
-
-    const fullNameDiv = document.createElement("div");
-    fullNameDiv.classList.add("full-name");
-    fullNameDiv.innerText = "Nama Lengkap";
+    // console.log(`Nama Lengkap: ${fullNameInput}`);
+    // console.log(`Nomor Induk Mahasiswa: ${studentIdInput}`);
+    // console.log(`Program Studi: ${majorInput}`);
+    // console.log(`\n`);
 
     const fullNameP = document.createElement("p");
-
-    const studentIdDiv = document.createElement("div");
-    studentIdDiv.classList.add("student-id");
-    studentIdDiv.innerText = "Nomor Induk Mahasiswa";
-
     const studentIdP = document.createElement("p");
-
-    const majorDiv = document.createElement("div");
-    majorDiv.classList.add("major");
-    majorDiv.innerText = "Program Studi";
-
     const majorP = document.createElement("p");
 
     fullNameP.textContent = `${fullNameInput}`;
